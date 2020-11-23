d4946febd396558043b7475ab254b6894b5b715f
Levi Lesches <levilesches@gmail.com>
2020-11-23 15:13:31 -0500

    Ignored lint one_member_abstracts for lib/src/services/service.dart

--- lib/src/services/service.dart
+++ lib/src/services/service.dart
@@ -1,3 +1,9 @@
+// Dart doesn't like it if we have an abstract class with only one function. 
+// They'd rather we pass the function around. But in our case, we want a class
+// so we can have an instance to pass around with properties and whatnot. 
+// 
+// ignore_for_file: one_member_abstracts
+
 abstract class Service {
 	Future<void> init();
 }
