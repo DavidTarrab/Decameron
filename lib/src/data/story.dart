c952de245abebc59c62913ad5cbedf35dd31e365
Levi Lesches <levilesches@gmail.com>
2020-11-23 15:22:18 -0500

    Commented the data library

--- lib/src/data/story.dart
+++ lib/src/data/story.dart
@@ -1,13 +1,29 @@
+import "author.dart";
+
+/// A story told by the fireplace. 
 class Story {
+	/// The date and time when this story was told. 
 	final DateTime createdAt;
-	final String authorID;
+
+	/// The storyteller. 
+	final Author author;
+
+	/// The title of this story. 
 	final String title;
+
+	/// The first sentence of this story.
+	/// 
+	/// This does not necessarily have to be the first sentence spoken. Instead 
+	/// it should be a catchy one-liner to hook the audience. 
 	final String firstSentence;
+
+	/// The full transcript of the story. 
 	final String text;
 
-	Story.fromJson(Map<String, dynamic> json) : 
+	/// Reads a story from a JSON object. 
+	Story.fromJson(Map json) : 
 		createdAt = DateTime.parse(json ["createdAt"]),
-		authorID = json ["authorID"],
+		author = Author.fromJson(json ["author"]),
 		title = json ["title"],
 		firstSentence = json ["firstSentence"],
 		text = json ["text"];
