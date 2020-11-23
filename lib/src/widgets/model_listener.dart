eb8f0af0455c8c9ffd47b847137783abdc81452a
Levi Lesches <levilesches@gmail.com>
2020-11-23 16:01:24 -0500

    Documented the widgets library

--- lib/src/widgets/model_listener.dart
+++ lib/src/widgets/model_listener.dart
@@ -1,14 +1,27 @@
 import "package:flutter/material.dart";
 
+/// A [WidgetBuilder], but with a [ChangeNotifier] and optional child. 
 typedef ModelBuilder<Model extends ChangeNotifier> = 
 	Widget Function(BuildContext context, Model model, Widget child);
 
+/// A widget that rebuilds when a data model updates. 
 class ModelListener<Model extends ChangeNotifier> extends StatefulWidget {
+	/// A function to build the widget tree using the model. 
 	final ModelBuilder<Model> builder;
+
+	/// A function to create the model. 
 	final Model Function() model;
+
+	/// An optional child. 
+	/// 
+	/// If a part of the widget tree does not depend on the model, you can pass
+	/// it to this widget as the child parameter to cache it. 
 	final Widget child;
+
+	/// Whether this widget should dispose the model when the widget is disposed. 
 	final bool shouldDispose;
 
+	/// Creates a widget that updates when the underlying data changes. 
 	const ModelListener({
 		@required this.model,
 		@required this.builder,
@@ -20,11 +33,17 @@ class ModelListener<Model extends ChangeNotifier> extends StatefulWidget {
 	ModelListenerState createState() => ModelListenerState<Model>();
 }
 
+/// The state for a [ModelListener]. 
+/// 
+/// This class is in charge of creating and disposing the model, as well 
+/// as listening to it for updates. 
 class ModelListenerState<Model extends ChangeNotifier>
 	extends State<ModelListener<Model>> 
 {
-	ChangeNotifier model;
+	/// The model representing the underlying data. 
+	Model model;
 
+	/// Rebuilds the widget tree when the underlying data changes. 
 	void listener() => setState(() {});
 
 	@override
