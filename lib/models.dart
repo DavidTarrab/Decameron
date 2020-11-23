10f7d030a211c4f24e4e5649b397d39392270d63
Levi Lesches <levilesches@gmail.com>
2020-11-23 13:57:25 -0500

    Implemented Models

---
+++ lib/models.dart
@@ -0,0 +1,17 @@
+import "src/models/model.dart";
+import "src/models/stories.dart";
+
+class Models extends Model {
+	static Models instance = Models();
+
+	final Stories stories = Stories();
+
+	@override
+	Future<void> init() => stories.init();
+
+	@override
+	void dispose() {
+		stories.dispose();
+		super.dispose();
+	}
+}
