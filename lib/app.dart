00f811b11051531ae812c24c458118e68ef756f9
Levi Lesches <levilesches@gmail.com>
2020-11-23 16:03:45 -0500

    Documented app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -12,17 +12,22 @@ class Decameron extends StatefulWidget {
   DecameronState createState() => DecameronState();
 }
 
+/// Initializes the services and data models for the app. 
 class DecameronState extends State<Decameron> {
+  /// Whether the data is ready for the app to use. 
   bool isReady = false;
 
   @override
   void initState() {
     super.initState();
+    // [init] is a Future, but this function is not async. 
+    // So we have to use the `then` callback. 
     init().then(
       (void _) => setState(() => isReady = true)
     );
   }
 
+  /// Initializes the services and data models. 
   Future<void> init() async {
     await Services.instance.init();
     await Models.instance.init();
