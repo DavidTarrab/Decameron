5888bd04583e27cddcaa1e53743280dc5ab687e8
Levi Lesches <levilesches@gmail.com>
2020-11-25 10:51:55 -0500

    Added uploading (and UserTile) to the home page

--- lib/src/widgets/story_spawner.dart
+++ lib/src/widgets/story_spawner.dart
@@ -3,8 +3,10 @@ import "package:flutter/material.dart";
 
 import "package:decameron/data.dart";
 import "package:decameron/pages.dart";
+import "package:decameron/services.dart";
 
 import "story_sentence.dart";
+import "user_tile.dart";
 
 /// Spawns [StorySentence] widgets over a fireplace. 
 class StorySpawner extends StatefulWidget {
@@ -51,6 +53,18 @@ class StorySpawnerState extends State<StorySpawner> {
 	@override
 	Widget build(BuildContext context) => Stack(
 		children: [
+			Container(
+				alignment: Alignment.topRight,
+				padding: const EdgeInsets.all(10),
+				width: 350,
+				child: Container(
+					decoration: BoxDecoration(
+						border: Border.all(color: Colors.white),
+						borderRadius: BorderRadius.circular(10),
+					),
+					child: UserTile()
+				)
+			),
 			Center(
 				child: Image.network(
 					"https://media3.giphy.com/media/3o6ZsWmtAVZFltTWVi/giphy.gif",
@@ -59,12 +73,14 @@ class StorySpawnerState extends State<StorySpawner> {
 			),
 			Align(
 				alignment: const Alignment(0, 0.85),
-				child: TextButton(
-					onPressed: () => Navigator.of(context).pushNamed(Routes.upload),
-					child: Text(
-						"Tell your own story", 
-						style: Theme.of(context).textTheme.headline5
-					),
+				child: Builder(
+					builder: (BuildContext context) => TextButton(
+						onPressed: uploadStory,
+						child: Text(
+							"Tell your own story", 
+							style: Theme.of(context).textTheme.headline5
+						),
+					)
 				)
 			),
 			for (final String sentence in sentences)
@@ -83,5 +99,15 @@ class StorySpawnerState extends State<StorySpawner> {
 		if (++index == widget.stories.length) {
 			index = 0;
 		}
+	}
+
+	/// Directs the user to upload a story, signing them in if necessary. 
+	Future<void> uploadStory() async {
+		if (!Services.instance.auth.isSignedIn) {
+			return Scaffold.of(context).showSnackBar(
+				const SnackBar(content: Text("You must be signed in to upload a story")),
+			);
+		}
+		await Navigator.of(context).pushNamed(Routes.upload);
 	}
 }
