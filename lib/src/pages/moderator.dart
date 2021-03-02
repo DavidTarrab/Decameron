24fc47c9e0644b1f57b7b994e51dcfb5929e4130
Levi Lesches <levilesches@gmail.com>
2021-03-02 14:56:23 -0500

    Added ModeratorPage

---
+++ lib/src/pages/moderator.dart
@@ -0,0 +1,48 @@
+import "package:flutter/material.dart";
+
+import "package:decameron/data.dart";
+import "package:decameron/models.dart";
+
+import "story.dart";
+
+class ModeratorPage extends StatefulWidget {
+	@override
+	ModeratorPageState createState() => ModeratorPageState();
+}
+
+class ModeratorPageState extends State<ModeratorPage> {
+	@override
+	Widget build(BuildContext context) => Scaffold(
+		appBar: AppBar(title: const Text("Moderator Page")),
+		body: FutureBuilder<List<Story>>(
+			future: Models.instance.moderator.pendingStories,
+			builder: (_, AsyncSnapshot snapshot) => !snapshot.hasData
+				? const Center(child: CircularProgressIndicator())
+				: snapshot.data.length == 0
+					? const Center(child: Text("No stories are pending approval."))
+					: ListView(
+						children: [
+							for (final Story story in snapshot.data)
+								UnapprovedStoryWidget(story)
+						]
+					)
+			)
+	);
+}
+
+class UnapprovedStoryWidget extends StatelessWidget {
+	final Story story;
+	const UnapprovedStoryWidget(this.story);
+
+	@override
+	Widget build(BuildContext context) => ListTile(
+		title: Text(story.title),
+		subtitle: Text(story.author.name),
+		onTap: () => Navigator.of(context).push(
+			MaterialPageRoute(
+				builder: (_) => StoryPage(story)
+			)
+		)
+	);
+}
+
