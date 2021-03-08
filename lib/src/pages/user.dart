247030559709eb3fa06c0a2500b97912ea5e0e4d
Levi Lesches <levilesches@gmail.com>
2021-03-08 11:42:07 -0500

    Started UserPage

--- lib/src/pages/user.dart
+++ lib/src/pages/user.dart
@@ -1,6 +1,28 @@
 import "package:flutter/material.dart";
+import "package:decameron/models.dart";
 
 class UserPage extends StatelessWidget {
 	@override
-	Widget build(BuildContext context) => Scaffold();
+	Widget build(BuildContext context) => Scaffold(
+		appBar: AppBar(title: const Text("User profile")),
+		body: Center(
+			child: Column(
+				children: [
+					const SizedBox(height: 10),
+					Text(
+						"Welcome, ${Models.instance.user.author.name}",
+						style: Theme.of(context).textTheme.headline4,
+					),
+					const SizedBox(height: 50),
+					// Text("Here are your stories"),
+				]
+			)
+		)
+	);
 }
+
+class StoryTile extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) => ListTile();
+}
+
